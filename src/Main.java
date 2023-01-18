f63ce8612006b1a9cc31c95dbac3f22dda6c6faa
Eduard <eduard.prog22@gmail.com>
2023-01-18 18:14:41 +0100

    First try

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = "Eduard";
+        System.out.println(myName);
+
+        if(myName.length() % 2 == 0){
+            System.out.println("Name length is even! " + myName.length());
+        } else{
+            System.out.println("Name length is odd! " + myName.length() );
+        }
+
+
+
+
+
+
+    }
+}
